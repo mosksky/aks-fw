d089003ed6e50dc52594ab46cacb4a2bb4548af4
Len Volk <mosksky@gmail.com>
2019-06-19 02:35:33 +0000

    added git commands

---
+++ git.ps1
@@ -0,0 +1,9 @@
+3 git init
+   4 git remote add origin https://github.com/mosksky/aks-fw.git
+   5 git status
+   6 git add .
+   7 git commit -m"init"
+   8 git config --global user.email "mosksky@gmail.com"
+   9 git config --global user.name "Len Volk"
+  10 git commit -m"init"
+  11 git push origin master
