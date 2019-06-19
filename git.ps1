58d92c167e9b7c767be04a4ad501ef73016cd785
Len Volk <mosksky@gmail.com>
2019-06-19 12:49:48 +0000

    added docker file and source app

--- git.ps1
+++ git.ps1
@@ -5,5 +5,4 @@
    7 git commit -m"init"
    8 git config --global user.email "mosksky@gmail.com"
    9 git config --global user.name "Len Volk"
-  10 git commit -m"init"
   11 git push origin master
